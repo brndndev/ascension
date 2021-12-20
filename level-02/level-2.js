0b23a53b69c4da69512f685f67d173ab967c97f6
Brndndev <brndndev@outlook.com>
2021-12-19 20:14:49 -0500

    completed level-02✅

---
+++ level-02/level-2.js
@@ -0,0 +1,6 @@
+let name = "Brandon"
+let color = "orange"
+let powers = "pyrokenesis"
+
+
+console.log(`Hello, my name is ${name} and my favorite color is ${color}  I'm also a superhero and my strongest power is  ${powers}`)
