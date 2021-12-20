49ca4eb13f716b37dfc5f6a06d76277c34d983f6
Brndndev <brndndev@outlook.com>
2021-12-20 16:01:58 -0500

    completed level-6✅

---
+++ level-06/level-6.js
@@ -0,0 +1,9 @@
+const ourList = []
+function addToArr(a){
+    ourList.push(a)
+    return ourList
+}
+console.log(addToArr(100))
+console.log(addToArr(1))
+console.log(addToArr(80))
+console.log(addToArr(18))
